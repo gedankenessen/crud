ca149f504111394641f2aff1ca7604f772bb1df6
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2022-11-14 17:49:08 +0100

    Make main start server

--- src/crud/core.clj
+++ src/crud/core.clj
@@ -1,8 +1,7 @@
 (ns crud.core
-  (:gen-class))
+  (:gen-class)
+  (:require [crud.entrypoint :as entrypoint]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn -main [& args]
+  entrypoint/start-server)
 
