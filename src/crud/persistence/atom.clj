dd616eab55df84e4ab570f37649acfe5471fc8de
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2022-11-24 16:33:10 +0100

    Add atom based implementation for local dev

--- src/crud/persistence/atom.clj
+++ src/crud/persistence/atom.clj
@@ -3,30 +3,87 @@
   (:import org.bson.types.ObjectId [com.mongodb MongoOptions ServerAddress]))
 
 (def storage
-  (atom {:63691793518fa064ce036c0c
-         {:focus
-          {:data
-           {:636a75a36a263c5cff4da190 {:x 1 :y 0}}}}}))
+  (atom {}))
+
+(comment
+  ;; Example structure of `storage`
+  (def example
+    {:63691793518fa064ce036c0c
+     {:focus
+      {:data
+       {:636a75a36a263c5cff4da190 {:x 1 :y 0}}}}})
+  (def user "63691793518fa064ce036c0c")
+  (def endpoint "focus"))
+
+(defn get-data [atom user endpoint]
+  (if-let [result (get-in @atom [(keyword user) (keyword endpoint) :data])]
+    [(map (fn [[k v]] (assoc v :id (name k))) result) nil]
+    [nil {:message (str "Could not find endpoint /" endpoint) :status 404}]))
+
+(defn get-data-by-id [atom user endpoint id]
+  (if-let [result (get-in @atom [(keyword user) (keyword endpoint) :data (keyword id)])]
+    [(assoc result :id (name id)) nil]
+    [nil {:message (str "Could not find item on /" endpoint " with id " id) :status 404}]))
+
+(defn get-data-last [atom user endpoint]
+  (if-let
+      [result
+       (first
+        (map
+         (fn [[k v]] (assoc v :id (name k)))
+         (get-in @atom [(keyword user) (keyword endpoint) :data])))]
+    [result nil]
+    [nil {:message (str "Could not find endpoint /" endpoint ".") :status 404}]))
+
+(defn add-endpoint [atom user endpoint new-data]
+  (let [id (ObjectId.)]
+    (swap!
+     atom
+     assoc-in
+     [(keyword user) (keyword endpoint)]
+     {:data (assoc {} (keyword (str id)) new-data)
+      :name endpoint
+      :methods []
+      :userId (ObjectId. user)
+      :timestamp (quot (System/currentTimeMillis) 1000)})
+    [{:id id} nil]))
+
+(defn add-data [atom user endpoint new-data]
+  (let [id (ObjectId.)]
+    (swap! atom assoc-in [(keyword user) (keyword endpoint) :data (keyword (str id))] new-data)
+    [{:id id} nil]))
+
+(defn add-version [atom user endpoint new-data]
+  (let [id (ObjectId.)]
+    (swap! atom assoc-in [(keyword user) (keyword endpoint)] {:data (assoc {} (keyword (str id)) new-data)})
+    [{:id id} nil]))
+
+(defn delete-data-by-id [atom user endpoint id]
+  (do
+    (swap!
+     atom
+     update-in
+     [(keyword user) (keyword endpoint) :data]
+     dissoc
+     (keyword id))
+    [{:id id} nil]))
+
+(defn update-data-by-id [atom user endpoint id data]
+  (do
+    (swap!
+     atom
+     assoc-in
+     [(keyword user) (keyword endpoint) :data (keyword id)]
+     data)
+    [{:id id} nil]))
 
 (defrecord Atom-Driver [atom]
   Persistence
-  ;; TODO: Handling errors
-  ;; TODO: Implement remaining
-  (get-data-last [atom user endpoint]
-    (if-let [result (first
-                     (map
-                      (fn [[k v]] (assoc v :id (name k)))
-                      (:data (get (get @atom (keyword user)) (keyword endpoint)))))]
-      [result nil]
-      ;; TODO: Could also be user
-      [nil {:message (str "Could not find endpoint /" endpoint ".") :status 404}]))
-  (add-data [config user endpoint new-data]
-    [nil {:message "Not implemented" :status 501}])
-  (add-version [db user endpoint new-data]
-    [nil {:message "Not implemented" :status 501}])
-  (delete-data-by-id [db user endpoint id]
-    [nil {:message "Not implemented" :status 501}])
-  (update-data-by-id [db user endpoint id data]
-    [nil {:message "Not implemented" :status 501}]))
-
-
+  (get-data [db user endpoint] get-data)
+  (get-data-last [db user endpoint] get-data-last)
+  (get-data-by-id [db user endpoint id] get-data-by-id)
+  (add-endpoint [db user endpoint new-data] add-endpoint)
+  (add-data [db user endpoint new-data] add-endpoint)
+  (add-version [db user endpoint new-data] add-endpoint)
+  (delete-data-by-id [db user endpoint id] delete-data-by-id)
+  (update-data-by-id [db user endpoint id data] update-data-by-id))
