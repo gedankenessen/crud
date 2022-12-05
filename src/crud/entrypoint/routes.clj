77e8ce4bdbbea1771fc7eca1e8762eaf8a4a5b8f
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2022-12-05 14:23:33 +0100

    Simplify authorization headers

--- src/crud/entrypoint/routes.clj
+++ src/crud/entrypoint/routes.clj
@@ -17,29 +17,35 @@
 
 (defroutes user-routes
   "Routes for user (token) related actions. Requires authorization header."
-  (context
-   "/user" []
-   (GET "/" [] (wrap-authorization (fn [{token :token}] (user/details db token))))
-   (PUT "/" [] (wrap-authorization (fn [{token :token body :body}] (user/details db token body))))
-   (DELETE "/" [] (wrap-authorization (fn [{token :token body :body}] (user/delete db token body))))
-   (POST "/token" [] (wrap-authorization (fn [{body :body}] (sign-token (:id body) config))))))
+  (wrap-routes
+   (context
+    "/user" []
+    (GET "/" [] (fn [{token :token}] (user/details db token)))
+    (PUT "/" [] (fn [{token :token body :body}] (user/details db token body)))
+    (DELETE "/" [] (fn [{token :token body :body}] (user/delete db token body)))
+    (POST "/token" [] (fn [{body :body}] (sign-token (:id body) config))))
+   wrap-authorization))
 
 (defroutes meta-routes
   "Meta routes to work with the endpoints themselves. Requires authorization header."
-  (context
-   "/meta/:id" [id]
-   (DELETE "/" [] (wrap-authorization (fn [_] (meta/delete-endpoint db id))))
-   (GET "/" [] (wrap-authorization (fn [_] (meta/get-endpoint db id))))
-   (PUT "/" [] (wrap-authorization (fn [{body :body}] (meta/update-endpoint db id body))))))
+  (wrap-routes
+   (context
+    "/meta/:id" [id]
+    (DELETE "/" [] (fn [_] (meta/delete-endpoint db id)))
+    (GET "/" [] (fn [_] (meta/get-endpoint db id)))
+    (PUT "/" [] (fn [{body :body}] (meta/update-endpoint db id body))))
+   wrap-authorization))
 
 ;; TODO: Refactor from `/endpoints` to `/build` or `/crud` ?
 (defroutes crud-routes
   "Business logic routes. Heart of crud. Requires authorization header."
-  (context
-   "/endpoints/:endpoint"
-   [endpoint]
-   (GET "/:id" [id] (wrap-authorization (fn [{user :token}] (logic/on-get-id db user endpoint id))))
-   (GET "/" [] (wrap-authorization (fn [{user :token}] (logic/on-get db user endpoint))))
-   (POST "/" [] (wrap-authorization (fn [{user :token body :body}] (logic/on-post db user endpoint body))))
-   (PUT "/:id" [id] (wrap-authorization (fn [{user :token body :body}] (logic/on-put db user endpoint id body))))
-   (DELETE "/:id" [id] (wrap-authorization (fn [{user :token body :body}] (logic/on-delete-by-id db user endpoint id))))))
+  (wrap-routes
+   (context
+    "/endpoints/:endpoint"
+    [endpoint]
+    (GET "/:id" [id] (fn [{user :token}] (logic/on-get-id db user endpoint id)))
+    (GET "/" [] (fn [{user :token}] (logic/on-get db user endpoint)))
+    (POST "/" [] (fn [{user :token body :body}] (logic/on-post db user endpoint body)))
+    (PUT "/:id" [id] (fn [{user :token body :body}] (logic/on-put db user endpoint id body)))
+    (DELETE "/:id" [id] (fn [{user :token body :body}] (logic/on-delete-by-id db user endpoint id))))
+   wrap-authorization))
