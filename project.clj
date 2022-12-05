966e0472c3fcb43efdb74a22905f0eab51ae3c4f
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2022-12-05 13:46:32 +0100

    Add ring-devel deps for hot reloading

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
                  [ring "1.9.4"]
                  [ring/ring-defaults "0.3.3"]
                  [ring/ring-json "0.5.1"]
+                 [ring/ring-devel "1.8.0"]
                  [ring-cors "0.1.13"]
                  [http-kit "2.5.3"]
                  [compojure "1.6.2"]
