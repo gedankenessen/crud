eff5e0241fd36befb6ed4c25dd54180abc961827
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2022-12-21 18:43:03 +0100

    Fix atom record not working because of wrong parameters

--- src/crud/persistence/atom/core.clj
+++ src/crud/persistence/atom/core.clj
@@ -4,28 +4,28 @@
             [crud.persistence.atom.user :refer :all]
             [crud.persistence.atom.meta :refer :all]))
 
-(defrecord Atom-Driver [atom]
+(defrecord Atom-Driver [db]
   Persistence
   ;; setup
-  (connect [initial] (atom initial))
+  (connect [db] db)
   ;; crud functions
-  (get-data [db user endpoint] (get-data db user endpoint))
-  (get-data-last [db user endpoint] (get-data-last db user endpoint))
-  (get-data-by-id [db user endpoint id] (get-data-by-id db user endpoint id))
-  (add-endpoint [db user endpoint new-data] (add-endpoint db user endpoint new-data))
-  (add-data [db user endpoint new-data] (add-endpoint db user endpoint new-data))
-  (add-version [db user endpoint new-data] (add-endpoint db user endpoint new-data))
-  (delete-data-by-id [db user endpoint id] (delete-data-by-id db user endpoint id))
-  (update-data-by-id [db user endpoint id new-data] (update-data-by-id db user endpoint id new-data))
+  (get-data [db user endpoint] (get-data (:db db) user endpoint))
+  (get-data-last [db user endpoint] (get-data-last (:db db) user endpoint))
+  (get-data-by-id [db user endpoint id] (get-data-by-id (:db db) user endpoint id))
+  (add-endpoint [db user endpoint new-data] (add-endpoint (:db db) user endpoint new-data))
+  (add-data [db user endpoint new-data] (add-data (:db db) user endpoint new-data))
+  (add-version [db user endpoint new-data] (add-version (:db db) user endpoint new-data))
+  (delete-data-by-id [db user endpoint id] (delete-data-by-id (:db db) user endpoint id))
+  (update-data-by-id [db user endpoint id new-data] (update-data-by-id (:db db) user endpoint id new-data))
   ;; user functions
-  (get-user-by-email [db email] (get-user-by-email db email))
-  (get-user-by-id [db userId] (get-user-by-id db userId))
-  (add-user [db data] (add-user db data))
-  (update-user [db userId data] (update-user (update-user db userId data)))
-  (delete-user [db userId] (delete-user db userId))
+  (get-user-by-email [db email] (get-user-by-email (:db db) email))
+  (get-user-by-id [db userId] (get-user-by-id (:db db) userId))
+  (add-user [db data] (add-user (:db db) data))
+  (update-user [db userId data] (update-user (update-user (:db db) userId data)))
+  (delete-user [db userId] (delete-user (:db db) userId))
   ;; meta functions
-  (get-endpoints [db userId] (get-endpoints db userId))
-  (get-endpoint-by-id [db userId endpointId] (get-endpoint-by-id db userId endpointId))
-  (delete-endpoint-by-id [db userId endpointId] (delete-endpoint-by-id db userId endpointId))
-  (delete-endpoints-by-userId [db userId] (delete-endpoints-by-userId db userId))
-  (update-endpoint-by-id [db userId endpointId data] (update-endpoint-by-id db userId endpointId data)))
+  (get-endpoints [db userId] (get-endpoints (:db db) userId))
+  (get-endpoint-by-id [db userId endpointId] (get-endpoint-by-id (:db db) userId endpointId))
+  (delete-endpoint-by-id [db userId endpointId] (delete-endpoint-by-id (:db db) userId endpointId))
+  (delete-endpoints-by-userId [db userId] (delete-endpoints-by-userId (:db db) userId))
+  (update-endpoint-by-id [db userId endpointId data] (update-endpoint-by-id (:db db) userId endpointId data)))
