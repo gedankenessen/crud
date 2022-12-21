774c92eadf63b6a57eee45d5687f76f0af66d48f
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2022-12-21 18:43:35 +0100

    Update atom config

--- src/crud/persistence/atom/config.clj
+++ src/crud/persistence/atom/config.clj
@@ -1,7 +1,9 @@
-(ns crud.persistence.atom.config)
+(ns crud.persistence.atom.config
+  (:require [crud.persistence.atom.core :refer [map->Atom-Driver]]))
 
 (def config
-  (atom {}))
+  (atom {:users {}
+         :endpoints {}}))
 
 (comment
   ;; Example structure of `storage` (without timestamp etc.)
@@ -17,7 +19,22 @@
    {:63691793518fa064ce036c0c
     {:focus
      {:methods []
-      :userId 63691793518fa064ce036c0c
+      :userId :63691793518fa064ce036c0c
       :timestamp 1671548395158
       :data {:636a75a36a263c5cff4da190 {:x 1 :y 0}}}}}})
+
+;; TODO: Add default-users/endpoints to cli
+(defn get-config
+  "Sets up structure that will be atomized by `connect` call"
+  ([]
+   (get-config {}))
+  ([{default-db :default-db
+     default-users :default-users
+     default-endpoints :default-endpoints}]
+   (map->Atom-Driver
+    {:db
+     (or default-db
+         (atom {:users (or default-users {})
+                :endpoints (or default-endpoints {})}))})))
+
 
